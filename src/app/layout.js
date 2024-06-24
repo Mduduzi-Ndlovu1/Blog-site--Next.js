34633c373bb5fdee66c9e013716c576c9680bdb0
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-06-24 19:16:14 +0200

    Components created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,28 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
+import Navbar from '@/components/navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+  title: 'League of Legendary Blog',
+  description: 'This is a blog dedicated to interesting interpretations of league of legends lore',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className='wrapper'>
+            <Navbar/>
+            {children}
+            <Footer/>
+          </div>
+
+        </div>
+        </body>
     </html>
   )
 }
