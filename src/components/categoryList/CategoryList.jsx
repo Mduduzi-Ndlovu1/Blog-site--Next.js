c22cd24437a25a8a52232025f9fb71436afcfcd1
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-06-25 21:52:06 +0200

    Category styling and responsive  completed

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import styles from './categoryList.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 const CategoryList = () => {
   return (
-    <div className={styles.container}>CategoryList</div>
+    <section className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+          <Link href="/blog?cat=lessons" className={`${styles.category} ${styles.lessons}`}>
+            <Image src="/style1.jpg" alt='style1' width={32} height={32} className={styles.image}/>
+            Philosophy
+          </Link>
+
+          <Link href="/blog?cat=growth" className={`${styles.category} ${styles.growth}`}>
+            <Image src="/growth.jpg" alt='style1' width={32} height={32} className={styles.image}/>
+            Development
+          </Link>
+          
+          <Link href="/blog?cat=teamwork" className={`${styles.category} ${styles.teamwork}`}>
+            <Image src="/teamwork.jpg" alt='style1' width={32} height={32} className={styles.image}/>
+            Collaboration Dynamics          </Link>
+          <Link href="/blog?cat=psychological" className={`${styles.category} ${styles.psychological}`}>
+            <Image src="/psychological.jpg" alt='style1' width={32} height={32} className={styles.image}/>
+            Psychological Insights
+          </Link>
+      </div>
+    </section>
   )
 }
 
