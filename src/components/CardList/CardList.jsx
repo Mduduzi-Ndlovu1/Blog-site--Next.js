a1d536e810b985e402337f7c55aac2cb31956893
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-06-25 22:05:35 +0200

    post cardlist and card including styling file created

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styles from './cardList.module.css'
 import Pagination from '../pagination/Pagination'
+import Image from 'next/image'
+import Card from '../card/card'
 const CardList = () => {
   return (
-    <div className={styles.container}>
-      CardList
+    <section className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+        <Card/>
       <Pagination/>
-    </div>
+    </section>
   )
 }
 
