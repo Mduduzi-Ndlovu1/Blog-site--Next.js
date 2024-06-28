7f6f1c5e2c5654c824bdfd40fed62bfda3bbb654
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-06-28 11:40:00 +0200

    Write page completed

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -4,16 +4,18 @@ import styles from './writePage.module.css'
 import Image from 'next/image'
 import ReactQuill from 'react-quill'
 import "react-quill/dist/quill.bubble.css"
+import { CiCirclePlus } from "react-icons/ci";
+
 const WritePage = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
 
   return (
     <main className={styles.container}>
-      <input type="text" placeholder='Title' />
+      <input type="text" placeholder='Title' className={styles.input }/>
       <div className={styles.editor}>
         <button className={styles.button} onClick={() => setOpen(!open)}>
-          <Image src="/plus.png" alt='plus' width={16} height={16} className="plus"/>
+          <Image src="/plus-icon.svg" alt="add" width={16} height={16}/>
         </button> 
         {open && (
           <div className={styles.add}>
@@ -21,16 +23,22 @@ const WritePage = () => {
               <Image src="/image.png" alt='close' width={16} height={16} className="close"/>
             </button>
             <button className={styles.addButton}>
-              <Image src="/external.png" alt='close' width={16} height={16} className="close"/>
+              <Image src="/External.png" alt='close' width={16} height={16} className="close"/>
             </button>
             <button className={styles.addButton}>
               <Image src="/video.png" alt='close' width={16} height={16} className="close"/>
             </button>
           </div>
         )}
-        <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder='Tell your story'/>
+        <ReactQuill 
+        className={styles.textArea}
+        theme="bubble" 
+        value={value} 
+        onChange={setValue} 
+        placeholder='Tell your story'/>
       </div>
-    </main>
+      <button className={styles.publish}>Publish</button>
+    </main> 
   )
 }
 
